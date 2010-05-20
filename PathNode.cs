5405783b9aa1d0c0e3dd1898a48d07d8cd95ddf6
glacasse <glacasse@fecd4277-e07b-6055-b965-12065802a5d0>
2010-05-20 20:15:54 +0000

    Added project source

---
+++ PathNode.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GridAStar
+{
+    /// <summary>
+    /// Represents a node in the pathfinding graph.
+    /// </summary>
+    public sealed class PathNode<TState>
+    {
+        #region Fields
+        private TState state;
+        private PathNode<TState> previousNode;
+        private float costFromSource;
+        private float estimatedTotalCost;
+        private bool isOpen;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the state associated with this node of the graph.
+        /// </summary>
+        public TState State
+        {
+            get { return state; }
+        }
+
+        /// <summary>
+        /// Gets the previous node, least costly node that lead to this one.
+        /// <c>Null</c> if this node is the source node.
+        /// </summary>
+        public PathNode<TState> PreviousNode
+        {
+            get { return previousNode; }
+        }
+
+        /// <summary>
+        /// Gets the movement cost from the source node, passing through all previous nodes.
+        /// </summary>
+        public float CostFromSource
+        {
+            get { return costFromSource; }
+        }
+
+        /// <summary>
+        /// Gets the optimistic estimated cost to the destination.
+        /// </summary>
+        public float EstimatedCostToDestination
+        {
+            get { return estimatedTotalCost - costFromSource; }
+        }
+
+        /// <summary>
+        /// Gets the estimated total cost of this node,
+        /// the sum of the cost from the source and the estimated cost to the destination.
+        /// </summary>
+        public float EstimatedTotalCost
+        {
+            get { return estimatedTotalCost; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating if this node is in the open list.
+        /// </summary>
+        public bool IsOpen
+        {
+            get { return isOpen; }
+        }
+
+        /// <summary>
+        /// Gets the value indicating if this node is in the closed list.
+        /// </summary>
+        public bool IsClosed
+        {
+            get { return !isOpen; }
+        }
+        #endregion
+
+        #region Methods
+        internal void Reset(TState state, PathNode<TState> previousNode, float costFromSource, float estimatedCostToDestination)
+        {
+            this.state = state;
+            this.previousNode = previousNode;
+            this.costFromSource = costFromSource;
+            this.estimatedTotalCost = costFromSource + estimatedCostToDestination;
+            this.isOpen = true;
+        }
+
+        internal void UpdatePreviousNode(PathNode<TState> newPreviousNode, float costFromSource)
+        {
+            float estimatedCostToDestination = EstimatedCostToDestination;
+            this.previousNode = newPreviousNode;
+            this.costFromSource = costFromSource;
+            this.estimatedTotalCost = costFromSource + estimatedCostToDestination;
+        }
+
+        internal void Close()
+        {
+            isOpen = false;
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tek.Graphics;
+using System.Diagnostics;
+using Tek.Text;
+using System.Net;
+using System.Xml;
+
+namespace GridAStar
+{
+    public sealed class Query : IPathfindingQuery<string>
+    {
+        const int maxCommonLinkCount = 500;
+
+        private readonly string sourcePageName;
+        private readonly string destinationPageName;
+        private readonly HashSet<string> destinationPageLinks;
+
+        public Query(string sourcePageName, string destinationPageName)
+        {
+            this.sourcePageName = sourcePageName;
+            this.destinationPageName = destinationPageName;
+            this.destinationPageLinks = new HashSet<string>(GetPageLinks(destinationPageName));
+        }
+
+        public string Source
+        {
+            get { return sourcePageName; }
+        }
+
+        public string Destination
+        {
+            get { return destinationPageName; }
+        }
+
+        public void GetAdjacentStates(PathNode<string> node, List<AdjacentState<string>> adjacentStates)
+        {
+            foreach (string pageLink in GetPageLinks(node.State))
+                adjacentStates.Add(new AdjacentState<string>(pageLink, 100));
+        }
+
+        public float EstimateCostToDestination(string pageName)
+        {
+            HashSet<string> pageLinks = new HashSet<string>(GetPageLinks(pageName));
+            if (pageLinks.Contains(destinationPageName)) return 0;
+
+            pageLinks.IntersectWith(destinationPageLinks);
+            int commonLinkCount = pageLinks.Count;
+            int cost = maxCommonLinkCount - commonLinkCount;
+
+            if (commonLinkCount >= 10) Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0:D3}: {1}".FormatInvariant(commonLinkCount, pageName));
+            if (commonLinkCount >= 10) Console.ResetColor();
+
+            return cost;
+        }
+
+        public static IEnumerable<string> GetPageLinks(string pageName)
+        {
+            string url = "http://en.wikipedia.org/w/api.php?action=query&titles={0}&prop=links&format=xml&pllimit=500"
+                .FormatInvariant(pageName.Replace(" ", "%20"));
+
+            List<string> pageLinks = new List<string>();
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.UserAgent = "Venujord/1.0";
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    XmlDocument document = new XmlDocument();
+                    document.Load(response.GetResponseStream());
+                    foreach (XmlAttribute attribute in document.SelectNodes("/api/query/pages/page/links/pl/@title"))
+                        pageLinks.Add(attribute.Value);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Exception: " + e.Message);
+                Console.ResetColor();
+            }
+
+            pageLinks.RemoveAll(s => s.Contains(":"));
+
+            return pageLinks;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pathfinder<string> pathfinder = new Pathfinder<string>();
+
+            Console.Write("Enter source page name: ");
+            string sourcePageName = Console.ReadLine();
+            Console.Write("Enter destination page name: ");
+            string destinationPageName = Console.ReadLine();
+
+            Query query = new Query(sourcePageName, destinationPageName);
+            string[] path = pathfinder.Find(query);
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Path found!");
+            foreach (string pageName in path)
+                Console.WriteLine(pageName);
+            Console.WriteLine(query.Destination);
+
+            Console.Read();
+        }
+    }
+}
